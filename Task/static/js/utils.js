257daf77aad57a0eafa17c1d99fef8d7727af236
Angela Radulescu <angelar@princeton.edu>
2015-01-21 09:32:06 -0500

    fixed bug in repeatelem

--- Task/static/js/utils.js
+++ Task/static/js/utils.js
@@ -23,7 +23,7 @@ function boolpercent(arr) {
 var repeatelem = function(elem, n){
         
         var arr = [];
-        for (var i=0; i<=n; i++) {
+        for (var i=0; i<n; i++) {
             arr = arr.concat(elem);
         };
         return arr;
@@ -32,4 +32,24 @@ var repeatelem = function(elem, n){
 // returns random number between min and max
 function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
+}
+
+// Fisher-Yates (aka Knuth) Shuffle
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
 }
